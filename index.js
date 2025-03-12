8f237a224a2b2e8ee50686bb52720c6c23ee39c0
Alex <alex.mani@student.moringaschool.com>
2025-03-12 15:40:39 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// code your solution here
+function superbowlWin(record) {
+    const win = record.find(season => season.result === "W");
+    return win ? win.year : undefined;
+  }
+  console.log(superbowlWin(record));
